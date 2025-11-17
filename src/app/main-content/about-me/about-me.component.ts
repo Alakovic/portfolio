048c251deee54505ff1a5191aa1ed3fe81e4908c
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-11-17 21:19:35 +0100

    About me finished

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
