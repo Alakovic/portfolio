4da8c0ee2aae8c9019e625a13ea30445507762bf
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-11-18 18:11:08 +0100

    technologies componemt added

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule,LandingPageComponent,AboutMeComponent],
+  imports: [CommonModule,LandingPageComponent,AboutMeComponent,TechnologiesComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
---
+++ src/app/main-content/technologies/technologies.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Technology } from '../../interfaces/technology_interface/technology.interface';
+
+@Component({
+  selector: 'app-technologies',
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
+  templateUrl: './technologies.component.html',
+  styleUrls: ['./technologies.component.scss'],
+})
+export class TechnologiesComponent {
+  technologies: Technology[] = [
+    { name: 'HTML', path: 'assets/images/technologies/html.png' },
+    { name: 'CSS', path: 'assets/images/technologies/css.png' },
+    { name: 'JavaScript', path: 'assets/images/technologies/js.png' },
+    { name: 'Material Design', path: 'assets/images/technologies/mat_design.png' },
+    { name: 'TypeScript', path: 'assets/images/technologies/ts.png' },
+    { name: 'Angular', path: 'assets/images/technologies/angular.png' },
+    { name: 'Firebase', path: 'assets/images/technologies/firebase.png' },
+    { name: 'Git', path: 'assets/images/technologies/git.png' },
+    { name: 'REST-API', path: 'assets/images/technologies/restApi.png' },
+    { name: 'Scrum', path: 'assets/images/technologies/scrum.png' },
+    { name: 'Growth mindset', path: 'assets/images/technologies/growth.png' }
+  ];
+}
