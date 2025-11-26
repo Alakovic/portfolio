c7f622e420ed2d1d79eb4c0153b8c7b2ef25e796
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-11-26 19:14:30 +0100

    project overlay almost finished, miss few adjsutements

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,54 +1,75 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Project } from '../../interfaces/project_interface/project.interface';
+import { SingleProjectComponent } from './single-project/single-project.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SingleProjectComponent],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
   projects: Project[] = [
-    { 
-      id:'01',
+    {
+      id: '01',
       name: 'Join',
-      tech:[
-        {name:'HTML', image:'assets/images/tech_overlay/html_overlay.png'},
-        {name:'CSS', image:'assets/images/tech_overlay/css_overlay.png'},
-        {name:'JavScript', image:'assets/images/tech_overlay/js_overlay.png'},
-        {name:'Firebase', image:'assets/images/tech_overlay/firebase_overlay.png'}
+      tech: [
+        { name: 'HTML', image: 'assets/images/tech_overlay/html_overlay.png' },
+        { name: 'CSS', image: 'assets/images/tech_overlay/css_overlay.png' },
+        {
+          name: 'JavScript',
+          image: 'assets/images/tech_overlay/js_overlay.png',
+        },
+        {
+          name: 'Firebase',
+          image: 'assets/images/tech_overlay/firebase_overlay.png',
+        },
       ],
       img: 'assets/images/projects/join.png',
-      description:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions,assign users and categories.',
-      github:'https://github.com/Alakovic/join_v2',
-      demo:'https://zeljko-alakovic.developerakademie.net/join_v2/'
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions,assign users and categories.',
+      github: 'https://github.com/Alakovic/join_v2',
+      demo: 'https://zeljko-alakovic.developerakademie.net/join_v2/',
     },
     {
-      id:'02',
+      id: '02',
       name: 'Sharky',
       tech: [
-        {name:'HTML',  image:'assets/images/tech_overlay/html_overlay.png'},
-        {name:'CSS', image:'assets/images/tech_overlay/css_overlay.png'},
-        {name:'JavaScript',  image:'assets/images/tech_overlay/js_overlay.png'},
+        { name: 'HTML', image: 'assets/images/tech_overlay/html_overlay.png' },
+        { name: 'CSS', image: 'assets/images/tech_overlay/css_overlay.png' },
+        {
+          name: 'JavaScript',
+          image: 'assets/images/tech_overlay/js_overlay.png',
+        },
       ],
       img: 'assets/images/projects/sharky.png',
-      description:'Game based on object-oriented approach.Help Sharky to get trough various enemies , find all coins and poisoned elixir to fight against the big crazy shark',
-      github:'https://github.com/Alakovic/Sharky---THE-GAME',
-      demo:'https://zeljko-alakovic.developerakademie.net/Sharky---THE-GAME/'
+      description:
+        'Game based on object-oriented approach.Help Sharky to get trough various enemies , find all coins and poisoned elixir to fight against the big crazy shark',
+      github: 'https://github.com/Alakovic/Sharky---THE-GAME',
+      demo: 'https://zeljko-alakovic.developerakademie.net/Sharky---THE-GAME/',
     },
   ];
 
   currentImg: string = '';
-  imgIndex: number = 0
+  imgIndex: number = 0;
+  selectedProject: Project | null = null;
 
-  showImg(path: string,index:number) {
+  showImg(path: string, index: number) {
     this.currentImg = path;
-     this.imgIndex = index;
+    this.imgIndex = index;
   }
 
   hideImg() {
     this.currentImg = '';
+  }
+
+  openOverlay(p: Project) {
+    this.selectedProject = p;
+  }
+
+  closeOverlay() {
+    this.selectedProject = null; 
   }
 }
--- src/app/main-content/projects/single-project/single-project.component.ts
+++ src/app/main-content/projects/single-project/single-project.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component,Input } from '@angular/core';
+import { Component,Input,Output,EventEmitter } from '@angular/core';
 import { Project } from '../../../interfaces/project_interface/project.interface';
 
 @Component({
@@ -11,5 +11,10 @@ import { Project } from '../../../interfaces/project_interface/project.interface
 })
 export class SingleProjectComponent {
   @Input() project! : Project;
+  @Output() close = new EventEmitter<void>();
+
+  onClose(){
+    this.close.emit();
+  }
 
 }
