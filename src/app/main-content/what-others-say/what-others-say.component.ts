57d66a29c28248577efbe99bf4557fd7551e0e76
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-11-27 18:16:42 +0100

    references almost finished

--- src/app/main-content/what-others-say/what-others-say.component.ts
+++ src/app/main-content/what-others-say/what-others-say.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { WhatOthersSay } from '../../interfaces/others-say_interface/other-say.interface';
+import { SingleWOSComponent } from './single-wos/single-wos.component';
 
 @Component({
   selector: 'app-what-others-say',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SingleWOSComponent],
   templateUrl: './what-others-say.component.html',
   styleUrls: ['./what-others-say.component.scss'],
 })
@@ -17,9 +18,9 @@ export class WhatOthersSayComponent {
       text: 'I had the pleasure of working with Zeljko on the “JOIN” Project  , and the collaboration was truly smooth and professional. He was responsible for the Board, where he excellently implemented the display and logic for moving tasks through all stages, from “To Do” to “Done.” His dedication, precision, and teamwork greatly contributed to the quality of the final product.',
     },
     {
-      name: 'Kyrylo Novikov',
+      name: 'André Zelmer',
       role: 'Join Team Member',
-      text: 'I had the pleasure of working with Zeljko on the “JOIN” Project  , and the collaboration was truly smooth and professional. He was responsible for the Board, where he excellently implemented the display and logic for moving tasks through all stages, from “To Do” to “Done.” His dedication, precision, and teamwork greatly contributed to the quality of the final product.',
+      text: 'I worked with Zeljko on several projects. In the „Join“ project, he took over the area for managing tasks and delivered reliably. Technically very competent and absolutely uncomplicated as a person – that\'s exactly what you want in a team.',
     },
     {
       name: 'Kyrylo Novikov',
@@ -27,4 +28,14 @@ export class WhatOthersSayComponent {
       text: 'I had the pleasure of working with Zeljko on the “JOIN” Project  , and the collaboration was truly smooth and professional. He was responsible for the Board, where he excellently implemented the display and logic for moving tasks through all stages, from “To Do” to “Done.” His dedication, precision, and teamwork greatly contributed to the quality of the final product.',
     },
   ];
+
+  middleIndex = 0;
+
+  ngOnInit() {
+    this.middleIndex = Math.floor(this.whatOthersSay.length / 2);
+  }
+
+  goToSlide(index: number) {
+    this.middleIndex = index;
+  }
 }
