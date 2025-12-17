f1769cc36188984ae64e1913bd774fa3a759e8c9
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-12-17 15:23:05 +0100

    header footer references minor changes added

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule} from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,MatButtonModule,MatButtonToggleModule],
+  imports: [CommonModule,MatButtonModule,MatButtonToggleModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
