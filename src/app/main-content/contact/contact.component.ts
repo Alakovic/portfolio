41710e141dd0a74fe7b9baa08f7fe48898540053
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-12-17 17:30:36 +0100

    contact form finished, php file added , still need to be proper connected  at the end

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    agree: false ,
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if(ngForm.valid && this.contactData.agree)
+    console.log(this.contactData);
+    
+  }
 }
