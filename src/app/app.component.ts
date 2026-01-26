d9eb8c57acb59767d3a79e6347c00ad4e32f8eb7
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-01-26 14:08:53 +0100

    AOS Library installed and added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos'
 
 
 
@@ -15,5 +16,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  ngOnInit():void{
+    AOS.init();
+  }
 
 }
