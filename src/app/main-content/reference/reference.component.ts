f0cf51cce4c880bc7f3372e6d359937a823f5e5c
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-01-29 20:55:35 +0100

    carousel animation added

--- src/app/main-content/reference/reference.component.ts
+++ src/app/main-content/reference/reference.component.ts
@@ -3,40 +3,31 @@ import { Component } from '@angular/core';
 import { SingleReferenceComponent } from './single-reference/single-reference.component';
 import { Reference } from '../../interfaces/reference_interface/reference.interface';
 import { MatCardModule } from '@angular/material/card';
-import { TranslateService,TranslateModule } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reference',
   standalone: true,
-  imports: [CommonModule, SingleReferenceComponent, MatCardModule,TranslateModule],
+  imports: [
+    CommonModule,
+    SingleReferenceComponent,
+    MatCardModule,
+    TranslateModule,
+  ],
   templateUrl: './reference.component.html',
   styleUrls: ['./reference.component.scss'],
 })
 export class ReferenceComponent {
-  references$ = this.translate.stream('REFERENCES.REFERENCE')
+  references$ = this.translate.stream('REFERENCES.REFERENCE');
   references: Reference[] = [];
 
-  constructor(private translate:TranslateService) {
-    this.references$.subscribe((references:Reference[]) => {
-      this.references = references
-    })
+  constructor(private translate: TranslateService) {
+    this.references$.subscribe((references: Reference[]) => {
+      this.references = references;
+    });
   }
 
   activeIndex = 0;
-
-  get prevReference(): Reference {
-    return this.references[
-      (this.activeIndex - 1 + this.references.length) % this.references.length
-    ];
-  }
-
-  get currentReference(): Reference {
-    return this.references[this.activeIndex];
-  }
-
-  get nextReference(): Reference {
-    return this.references[(this.activeIndex + 1) % this.references.length];
-  }
 
   next() {
     this.activeIndex = (this.activeIndex + 1) % this.references.length;
@@ -49,5 +40,19 @@ export class ReferenceComponent {
 
   setIndex(i: number) {
     this.activeIndex = i;
+  }
+
+  getSlideClass(index: number): string {
+    if (index === this.activeIndex) return 'active';
+    if (
+      index ===
+      (this.activeIndex - 1 + this.references.length) % this.references.length
+    ) {
+      return 'left';
+    }
+    if (index === (this.activeIndex + 1) % this.references.length) {
+      return 'right';
+    }
+    return 'hidden';
   }
 }
